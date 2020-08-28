60bdf99eb69a33092356daa0fd9bb3f9ca5c99bf
rony <mylovepre16@gmail.com>
2020-08-28 15:44:01 -0500

    adding documentation email

--- src/routes/subscribeEmail.ts
+++ src/routes/subscribeEmail.ts
@@ -24,9 +24,41 @@ export default class SubscribeEMail {
    */
   public Endpoint(path: string): void {
     this.router
-      // metodo post
+      /**
+       * @api {post} /api/v1/subscribe-email Agregar lista de subscriptores.
+       * @apiName SetSubscribeAll
+       * @apiVersion 0.0.1
+       * @apiGroup Email
+       * @apiParam { string } email Email a agregar del cliente
+       * @apiSuccess {Boolean} status true
+       * @apiSuccess {String} message Mensaje de respuesta exitosa.
+       * @apiError EmailNotFOund   El <code>email</code> es necesario para esta consulta.
+       * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error.
+       * @apiSuccessExample {json} Succes-Example
+       * HTTP/1.1 200 OK
+       * {
+       *    status: true,
+       *    message: "La solicitud se resolvio con exito"
+       * }
+       */
       .post(path + "/subscribe-email", this.subscribeEMail.setSubscribeEmail)
-      // metodo get
+      /**
+       * @api {get} /api/v1/subscribe-email Solicitar lista de subscriptores.
+       * @apiName GetSubscribeAll
+       * @apiVersion 0.0.1
+       * @apiGroup Email
+       * @apiSuccess {Boolean} status true
+       * @apiSuccess {Object} data Subcribe Email list.
+       * @apiSuccess {String} message Mensaje de respuesta exitosa.
+       * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error.
+       * @apiSuccessExample {json} Succes-Example
+       * HTTP/1.1 200 OK
+       * {
+       *    status: true,
+       *    data: [{}],
+       *    message: "La solicitud se resolvio con exito"
+       * }
+       */
       .get(path + "/subscribe-email", this.subscribeEMail.getSubscribeEmails);
   }
 }
