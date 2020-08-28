cfb5323b5a13e812b8bf092b50ebc2cafe4ef9e9
rony <mylovepre16@gmail.com>
2020-08-28 15:44:23 -0500

    adding documentation analytics

--- src/routes/analyticsRoute.ts
+++ src/routes/analyticsRoute.ts
@@ -1,7 +1,5 @@
 /** @format */
 import express from "express";
-
-// [*] controlador para resolver las consultas.
 import analyticsController from "@controllers/analytics-controller";
 
 /**
@@ -9,7 +7,6 @@ import analyticsController from "@controllers/analytics-controller";
  * @see https://digitalnew.solutions/
  * @see https://github.com/DigitalNews/
  * @version 1.0.0
- * @process 2
  */
 export default class UserRouter {
   public router: express.Application;
@@ -24,8 +21,53 @@ export default class UserRouter {
    */
   public Endpoint(path: string): void {
     this.router
-    // metodo get
-    .get(path + "/analytics/users-by-country", this.analyticsController.getUserbyCountry)
-    .get(path + "/analytics/top-browsers/:viewid", this.analyticsController.getTopBrowsers)
+
+      /**
+       * @api {get} /api/v1/analytics/users-by-country Solicitar usuarios por paises
+       * @apiVersion 0.0.1
+       * @apiName GetUserByCountry
+       * @apiGroup Analytics
+       * @apiParam { string } viewid Identificación unica del proyecto a analizar de Google Analytics
+       * @apiSuccess {Boolean} status true.
+       * @apiSuccess {Object} data top User by country list.
+       * @apiSuccess {String} message Mensaje de respuesta exitosa.
+       * @apiError ViewIDNotFound   El <code>viewid</code> de google analytics no es valido.
+       * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error.
+       * @apiSuccessExample {json} Succes-Example
+       * HTTP/1.1 200 OK
+       * {
+       *    status: true,
+       *    data: [{}],
+       *    message: "La solicitud se resolvio con exito"
+       * }
+       */
+      .get(
+        path + "/analytics/users-by-country",
+        this.analyticsController.getUserbyCountry
+      )
+
+      /**
+       * @api {get} /api/v1/analytics/top-browsers/:viewid Solicitar tops navegadores
+       * @apiName GetTopBrowser
+       * @apiVersion 0.0.1
+       * @apiGroup Analytics
+       * @apiParam { string } viewid Identificación unica del proyecto a analizar de Google Analytics
+       * @apiSuccess {Boolean} status true.
+       * @apiSuccess {Object} data top Browser list.
+       * @apiSuccess {String} message Mensaje de respuesta exitosa.
+       * @apiError ViewIDNotFound   El <code>viewid</code> de google analytics no es valido.
+       * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error.
+       * @apiSuccessExample {json} Succes-Example
+       * HTTP/1.1 200 OK
+       * {
+       *    status: true,
+       *    data: [{}],
+       *    message: "La solicitud se resolvio con exito"
+       * }
+       */
+      .get(
+        path + "/analytics/top-browsers/:viewid",
+        this.analyticsController.getTopBrowsers
+      );
   }
 }
