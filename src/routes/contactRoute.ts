9b0b78e0bafa23b6f0d0c5a493f5fe7a463e3d59
rony <mylovepre16@gmail.com>
2020-08-28 15:44:11 -0500

    adding documentation CLient

--- src/routes/contactRoute.ts
+++ src/routes/contactRoute.ts
@@ -24,11 +24,60 @@ export default class UserRouter {
    */
   public Endpoint(path: string): void {
     this.router
-    // metodo post
-    .post(path + "/contact", this.userController.setContact)
-    // metodo get
-    .get(path + "/contact", this.userController.getContacts)
-    // metodo deleted
-    .delete(path + "/contact/:uid", this.userController.deletedContac)
+      /**
+       * @api {post} /api/v1/contact Agregar lista de clientes contacto.
+       * @apiName SetClientContact
+       * @apiVersion 0.0.1
+       * @apiGroup User
+       * @apiParam {String} name Client name contact
+       * @apiParam {String} message Client message contact
+       * @apiParam {String}  email Client email contact
+       * @apiSuccess {Boolean} status true.
+       * @apiSuccess {String} message Mensaje de respuesta exitosa.
+       * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error.
+       * @apiSuccessExample {json} Succes-Example
+       * HTTP/1.1 200 OK
+       * {
+       *    status: true,
+       *    message: "La solicitud se resolvio con exito"
+       * }
+       */
+      .post(path + "/contact", this.userController.setContact)
+      /**
+       * @api {get} /api/v1/contact Solicitar lista de Clientes.
+       * @apiName GetClientContact
+       * @apiVersion 0.0.1
+       * @apiGroup User
+       * @apiSuccess {Boolean} status true
+       * @apiSuccess {Object} data client contacts list.
+       * @apiSuccess {String} message Mensaje de respuesta exitosa.
+       * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error.
+       * @apiSuccessExample {json} Succes-Example
+       * HTTP/1.1 200 OK
+       * {
+       *    status: true,
+       *    data: [{}]
+       *    message: "La solicitud se resolvio con exito"
+       * }
+       */
+      .get(path + "/contact", this.userController.getContacts)
+      /**
+       * @api {delete} /api/v1/contact/:uid Solicitar Eliminar un cliente.
+       * @apiName DeleteClientContact
+       * @apiVersion 0.0.1
+       * @apiGroup User
+       * @apiParam {String} uid cliente uid
+       * @apiSuccess {Boolean} status true
+       * @apiSuccess {String} message Mensaje de respuesta exitosa.
+       * @apiError UserNotFound  The <code>uid</code> of the Client Contact was not found.
+       * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error.
+       * @apiSuccessExample {json} Succes-Example
+       * HTTP/1.1 200 OK
+       * {
+       *    status: true,
+       *    message: "La solicitud se resolvio con exito"
+       * }
+       */
+      .delete(path + "/contact/:uid", this.userController.deletedContac);
   }
 }
